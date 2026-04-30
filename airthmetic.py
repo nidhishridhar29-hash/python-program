8ee56c7d74f7b36746c8af4d84dadfa35ecf6093
nidhishridhar29-hash <nidhishridhar29@gmail.com>
2026-04-30 08:51:00 +0530

    Create airthmetic.py

---
+++ airthmetic.py
@@ -0,0 +1,21 @@
+a=int(input("Enter a value")); #input() takes data from console at runtime as string.
+b=int(input("Enter b value")); #typecast the input string to int.
+print("Addition of a and b ",a+b);
+print("Subtraction of a and b ",a-b);
+print("Multiplication of a and b ",a*b);
+print("Division of a and b ",a/b);
+print("Remainder of a and b ",a%b);
+print("Exponent of a and b ",a**b); #exponent operator (a^b)
+print("Floar division of a and b ",a//b); # floar division
+
+
+output
+Enter a value3
+Enter b value5
+Addition of a and b  8
+Subtraction of a and b  -2
+Multiplication of a and b  15
+Division of a and b  0.6
+Remainder of a and b  3
+Exponent of a and b  243
+Floar division of a and b  0
