320929e1df7fac974f8c645a3d0dd07a784d7f42
nidhishridhar29-hash <nidhishridhar29@gmail.com>
2026-04-30 08:53:43 +0530

    Create celcius.py

---
+++ celcius.py
@@ -0,0 +1,28 @@
+print("Options are \n")
+print("1.Convert temperatures from Celsius to Fahrenheit \n")
+print("2.Convert temperatures from Fahrenheit to Celsius \n")
+opt=int(input("Choose any Option(1 or 2) : "))
+if opt == 1:
+    print("Convert temperatures from Celsius to Fahrenheit \n")
+    cel = float(input("Enter Temperature in Celsius: "))
+    fahr = (cel*9/5)+32
+    print("Temperature in Fahrenheit =",fahr)
+elif opt == 2:
+    print("Convert temperatures from Fahrenheit to Celsius \n")
+    fahr = float(input("Enter Temperature in Fahrenheit: "))
+    cel=(fahr-32)*5/9;
+    print("Temperature in Celsius =",cel)
+else:
+    print("Invalid Option")
+
+output
+
+1.Convert temperatures from Celsius to Fahrenheit 
+
+2.Convert temperatures from Fahrenheit to Celsius 
+
+Choose any Option(1 or 2) : 2
+Convert temperatures from Fahrenheit to Celsius 
+
+Enter Temperature in Fahrenheit: 235
+Temperature in Celsius = 112.77777777777777
