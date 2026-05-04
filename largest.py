c1ace3eeee811c9c6e63fddb50726c3d184daaa8
nidhishridhar29-hash <nidhishridhar29@gmail.com>
2026-05-04 21:09:00 +0530

    Create largest.py

---
+++ largest.py
@@ -0,0 +1,21 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
+ 
+if (num1 > num2) and (num1 > num3):
+   largest = num1
+elif (num2 > num1) and (num2 > num3):
+   largest = num2
+else:
+   largest = num3
+ 
+print("The largest number is",largest)
+
+
+output
+r (4)/largest.py"
+Enter first number: 2
+Enter second number: 4
+Enter third number: 6
+The largest number is 6
+PS C:\Users\nidhi\Desktop\New folder (4)> 
