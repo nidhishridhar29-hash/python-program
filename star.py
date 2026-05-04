a2aa03147fb4090326b980b42ab21be6d0add8e9
nidhishridhar29-hash <nidhishridhar29@gmail.com>
2026-05-04 21:10:59 +0530

    Create star.py

---
+++ star.py
@@ -0,0 +1,25 @@
+n=5;
+for i in range(n):
+    for j in range(i):
+        print ('* ', end="")
+    print('')
+
+for i in range(n,0,-1):
+    for j in range(i):
+        print('* ', end="")
+    print('')
+
+output
+
+r (4)/nested.py"
+
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+PS C:\Users\nidhi\Desktop\New folder (4)> 
